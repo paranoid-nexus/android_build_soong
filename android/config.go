a9e4c7345b718dd940ed444838ea49c649df5389
CuriousNom <aryanbiswas7585@gmail.com>
2026-04-04 13:39:37 -0400

    soong: Treat certs/* as release-keys
    Change-Id: I6ef6559ece5589d6b0a6d150ff70f68ac7b5c74a

--- android/config.go
+++ android/config.go
@@ -1312,7 +1312,7 @@ func (c *config) BuildKeys() string {
 	if defaultCert == "" || defaultCert == filepath.Join(testKeyDir, "testkey") {
 		return "test-keys"
 	}
-	if strings.HasPrefix(defaultCert, "vendor/nexus-priv/") {
+	if strings.HasPrefix(defaultCert, "certs") {
 		return "release-keys"
 	}
 	return "dev-keys"
